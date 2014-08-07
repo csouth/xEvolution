edaf94fea33d280b8c3fa2d15f426e00c314d7cb
Christian South <csouth@xistins.com>
2014-08-06 20:45:23 -0400

    They're alive

---
+++ js/app.js
@@ -0,0 +1,43 @@
+function xEvolution(height, width) {
+    this.canvasHeight = height || 600;
+    this.canvasWidth  = width || 600;
+    this.canvas = this.createCanvas(this.canvasHeight, this.canvasWidth);
+    this.interval = null;
+
+    this.creatures = [];
+}
+
+xEvolution.prototype.createCanvas = function(height, width) {
+    var canvas    = document.createElement('canvas');
+    canvas.width  = width;
+    canvas.height = height;
+    document.body.appendChild(canvas);
+
+    return canvas.getContext('2d');
+};
+
+xEvolution.prototype.addCreature = function() {
+    var creature = new Creature();
+    this.creatures.push(new Creature());
+};
+
+xEvolution.prototype.tick = function() {
+    for(i=0; i < window.app.creatures.length; i++) {
+        window.app.creatures[i].doTick();
+    }
+};
+
+xEvolution.prototype.start = function() {
+    if(null === this.interval) {
+        this.interval = setInterval(this.tick, 100);
+    }
+};
+
+xEvolution.prototype.stop = function() {
+    if(null !== this.interval) {
+        clearInterval(this.interval);
+    }
+};
+
+window.app = new xEvolution();
+window.app.addCreature();
---
+++ js/creature.js
@@ -0,0 +1,52 @@
+function Creature(x, y, height, width) {
+    this.speed = 5;
+    this.movementSize = 2;
+    this.currentX = x || Math.floor((Math.random() * window.app.canvasWidth) + 1);
+    this.currentY = x || Math.floor((Math.random() * window.app.canvasHeight) + 1);
+    this.height = height || 3;
+    this.width = width || 3;
+}
+
+Creature.prototype.doTick = function() {
+    var randomNumber = Math.floor((Math.random() * 10) + 1);
+    if(randomNumber >= this.speed) {
+        this.doAction();
+        return;
+    }
+};
+
+Creature.prototype.doAction = function() {
+    var randomNumber = Math.floor((Math.random() * 100) + 1);
+
+    if(randomNumber <= 25) { // Go left one space
+        this.move(this.currentX - this.movementSize, this.currentY);
+    } else if(randomNumber <= 50) { // Go right one space
+        this.move(this.currentX + this.movementSize, this.currentY);
+    } else if(randomNumber <= 75) { // Go up one space
+        this.move(this.currentX, this.currentY - this.movementSize);
+    } else { // Go down one space
+        this.move(this.currentX, this.currentY + this.movementSize);
+    }
+};
+
+Creature.prototype.move = function(x, y) {
+
+    window.app.canvas.fillStyle = '#fff';
+    window.app.canvas.fillRect(this.currentX, this.currentY, this.height, this.width);
+
+    window.app.canvas.fillStyle = '#000';
+
+    if((x+this.width) > window.app.canvasWidth || (x-this.width) < 0) {
+        x = window.app.canvasWidth-this.width;
+    }
+
+    if((y+this.height) > window.app.canvasHeight || (x-this.height) < 0) {
+        y = window.app.canvasHeight - this.height;
+    }
+
+    window.app.canvas.fillRect(x, y, this.height, this.width);
+
+    this.currentX = x;
+    this.currentY = y;
+
+};
